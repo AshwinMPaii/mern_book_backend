f67feb259a95c44cbe4f92eceb7f7cf35449ab8c
Ashwin M Pai <ashwinpai808@gmail.com>
2023-10-08 00:35:05 +0530

    did something

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 export const PORT = process.env.PORT || 5555;
-export const MONGODB_URL = process.env.MONGODB_URL || "mongodb://localhost:27017/development-db";
+export const MONGODB_URL = process.env.MONGODB_URL || "mongodb+srv://ashwinpai808:b9QJKU7FlDmRdBzj@cluster0.dxg0w9b.mongodb.net/books-collection?retryWrites=true&w=majority";
